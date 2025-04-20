bf8ad1e63f65f39df33c6561f696f56b2f17ca12
Kai Jie <kaijie.chan@tum.de>
2025-04-20 17:30:58 +0200

    WIP

--- gnn/main.py
+++ gnn/main.py
@@ -1,109 +1,234 @@
-import argparse
-import os.path as osp
+import datetime
 import time
 
+import numpy as np
 import torch
-import torch.nn.functional as F
 
-import torch_geometric
-import torch_geometric.transforms as T
+np.random.seed(0)
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+# TODO Need to update values
+plt.rcParams['figure.dpi'] = 96
+plt.rcParams.update({'font.size': 24})
 from torch_geometric.datasets import Planetoid
-from torch_geometric.logging import init_wandb, log
-from torch_geometric.nn import GCNConv
+from torch_geometric.utils import degree
+from collections import Counter
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GATv2Conv
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='Cora')
-parser.add_argument('--hidden_channels', type=int, default=16)
-parser.add_argument('--lr', type=float, default=0.01)
-parser.add_argument('--epochs', type=int, default=200)
-parser.add_argument('--use_gdc', action='store_true', help='Use GDC')
-parser.add_argument('--wandb', action='store_true', help='Track experiment')
-args = parser.parse_args()
+"""
+This script is based on the example provided in https://mlabonne.github.io/blog/posts/2022-03-09-Graph_Attention_Network.html
+"""
 
-device = torch_geometric.device('auto')
+if __name__ == '__main__':
+    # TODO Revert
+    epoch = 10
+    # epoch = 200
+    # Defines the number of epochs between every animation frame
+    epochGran = 5
 
-init_wandb(
-    name=f'GCN-{args.dataset}',
-    lr=args.lr,
-    epochs=args.epochs,
-    hidden_channels=args.hidden_channels,
-    device=device,
-)
+    tic = time.time()
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
-dataset = Planetoid(path, args.dataset, transform=T.NormalizeFeatures())
-data = dataset[0].to(device)
+    """Import and analyse dataset"""
+    # Import dataset from PyTorch Geometric
+    dataset = Planetoid(root=".", name="CiteSeer")
+    data = dataset[0]
 
-if args.use_gdc:
-    transform = T.GDC(
-        self_loop_weight=1,
-        normalization_in='sym',
-        normalization_out='col',
-        diffusion_kwargs=dict(method='ppr', alpha=0.05),
-        sparsification_kwargs=dict(method='topk', k=128, dim=0),
-        exact=True,
-    )
-    data = transform(data)
+    # Get the list of degrees for each node
+    degrees = degree(data.edge_index[0]).numpy()
+    # Count the number of nodes for each degree
+    numbers = Counter(degrees)
+    # Convert counter to dictionary
+    numbers = {int(k): v for k, v in numbers.items()}
 
+    # Bar plot
+    fig, ax = plt.subplots(figsize=(18, 6))
+    ax.set_xlabel('Node Degree')
+    ax.set_ylabel('Number of Nodes')
+    plt.bar(numbers.keys(), numbers.values(), color='#0A047A')
+    plt.title("Node Degree Distribution")
+    plt.xlim(min(numbers.keys()), max(numbers.keys()))
+    plt.savefig('nodeDegree.png', bbox_inches="tight")
+    plt.show()
 
-class GCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
-        super().__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels,
-                             normalize=not args.use_gdc)
-        self.conv2 = GCNConv(hidden_channels, out_channels,
-                             normalize=not args.use_gdc)
+    # TODO Use tSNE visualisation on original dataset
 
-    def forward(self, x, edge_index, edge_weight=None):
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.conv1(x, edge_index, edge_weight).relu()
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.conv2(x, edge_index, edge_weight)
-        return x
+    """Create models"""
 
 
-model = GCN(
-    in_channels=dataset.num_features,
-    hidden_channels=args.hidden_channels,
-    out_channels=dataset.num_classes,
-).to(device)
+    class GCN(torch.nn.Module):
+        """Graph Convolutional Network"""
 
-optimizer = torch.optim.Adam([
-    dict(params=model.conv1.parameters(), weight_decay=5e-4),
-    dict(params=model.conv2.parameters(), weight_decay=0)
-], lr=args.lr)  # Only perform weight-decay on first convolution.
+        def __init__(self, dim_in, dim_h, dim_out):
+            super().__init__()
+            self.gcn1 = GCNConv(dim_in, dim_h)
+            self.gcn2 = GCNConv(dim_h, dim_out)
+            self.optimizer = torch.optim.Adam(self.parameters(),
+                                              lr=0.01,
+                                              weight_decay=5e-4)
 
-
-def train():
-    model.train()
-    optimizer.zero_grad()
-    out = model(data.x, data.edge_index, data.edge_attr)
-    loss = F.cross_entropy(out[data.train_mask], data.y[data.train_mask])
-    loss.backward()
-    optimizer.step()
-    return float(loss)
+        def forward(self, x, edge_index):
+            h = F.dropout(x, p=0.5, training=self.training)
+            h = self.gcn1(h, edge_index).relu()
+            h = F.dropout(h, p=0.5, training=self.training)
+            h = self.gcn2(h, edge_index)
+            return h, F.log_softmax(h, dim=1)
 
 
-@torch.no_grad()
-def test():
-    model.eval()
-    pred = model(data.x, data.edge_index, data.edge_attr).argmax(dim=-1)
+    class GAT(torch.nn.Module):
+        """Graph Attention Network"""
 
-    accs = []
-    for mask in [data.train_mask, data.val_mask, data.test_mask]:
-        accs.append(int((pred[mask] == data.y[mask]).sum()) / int(mask.sum()))
-    return accs
+        def __init__(self, dim_in, dim_h, dim_out, heads=8):
+            super().__init__()
+            self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
+            self.gat2 = GATv2Conv(dim_h * heads, dim_out, heads=1)
+            self.optimizer = torch.optim.Adam(self.parameters(),
+                                              lr=0.005,
+                                              weight_decay=5e-4)
+
+        def forward(self, x, edge_index):
+            h = F.dropout(x, p=0.6, training=self.training)
+            h = self.gat1(h, edge_index)
+            h = F.elu(h)
+            h = F.dropout(h, p=0.6, training=self.training)
+            h = self.gat2(h, edge_index)
+            return h, F.log_softmax(h, dim=1)
 
 
-best_val_acc = test_acc = 0
-times = []
-for epoch in range(1, args.epochs + 1):
-    start = time.time()
-    loss = train()
-    train_acc, val_acc, tmp_test_acc = test()
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    log(Epoch=epoch, Loss=loss, Train=train_acc, Val=val_acc, Test=test_acc)
-    times.append(time.time() - start)
-print(f'Median time per epoch: {torch.tensor(times).median():.4f}s')
+    def accuracy(pred_y, y):
+        """Calculate accuracy."""
+        return ((pred_y == y).sum() / len(y)).item()
+
+
+    def train(model, data):
+        """Train a GNN model and return the trained model."""
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = model.optimizer
+
+        # TODO Return embedding and output list
+        embeddings = []
+        outputs = []
+
+        model.train()
+        for epoch_ in range(epoch + 1):
+            # Training
+            optimizer.zero_grad()
+            _, out = model(data.x, data.edge_index)
+            loss = criterion(out[data.train_mask], data.y[data.train_mask])
+            acc = accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
+            loss.backward()
+            optimizer.step()
+
+            # Validation
+            val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
+            val_acc = accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
+
+            # Print metrics
+            if (epoch_ % epochGran == 0):
+                print(f'Epoch {epoch_:>3} | Train Loss: {loss:.3f} | Train Acc: '
+                      f'{acc * 100:>6.2f}% | Val Loss: {val_loss:.2f} | '
+                      f'Val Acc: {val_acc * 100:.2f}%')
+
+        return model
+
+
+    @torch.no_grad()
+    def test(model, data):
+        """Evaluate the model on the test set and print the accuracy score."""
+        model.eval()
+        _, out = model(data.x, data.edge_index)
+        acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
+        # TODO Use TSNE visualisation here after training and compare with results before training.
+        #  Check perplexity value which garners the lowest KL divergence
+        return acc
+
+
+    """Train and test models"""
+    # Create GCN model
+    gcn = GCN(dataset.num_features, 16, dataset.num_classes)
+    print(gcn)
+    # Train and test
+    train(gcn, data)
+    acc = test(gcn, data)
+    print(f'\nGCN test accuracy: {acc * 100:.2f}%\n')
+
+    # Create GAT model
+    gat = GAT(dataset.num_features, 8, dataset.num_classes)
+    print(gat)
+    # Train and test
+    train(gat, data)
+    acc = test(gat, data)
+    print(f'\nGAT test accuracy: {acc * 100:.2f}%\n')
+
+    """Visualise embeddings of untrained GAT"""
+    # Initialize a new untrained model
+    untrained_gat = GAT(dataset.num_features, 8, dataset.num_classes)
+    # Get embeddings
+    h, _ = untrained_gat(data.x, data.edge_index)
+    # Train TSNE
+    tsne = TSNE(n_components=2, learning_rate='auto', init='pca').fit_transform(h.detach())
+
+    # Plot TSNE
+    plt.figure(figsize=(10, 10))
+    plt.axis('off')
+    plt.scatter(tsne[:, 0], tsne[:, 1], s=50, c=data.y)
+    plt.title('Embedding at Epoch 0')
+    plt.savefig('embedEpoch0.png')
+    plt.show()
+
+    """Visualise embeddings of trained GAT"""
+    # Get embeddings
+    h, _ = gat(data.x, data.edge_index)
+    # Train TSNE
+    tsne = TSNE(n_components=2, learning_rate='auto', init='pca').fit_transform(h.detach())
+
+    # Plot TSNE
+    plt.figure(figsize=(10, 10))
+    plt.axis('off')
+    plt.scatter(tsne[:, 0], tsne[:, 1], s=50, c=data.y)
+    plt.title('Embedding at Epoch ' + str(epoch))
+    plt.savefig('embedEpoch' + str(epoch) + '.png')
+    plt.show()
+
+    # TODO Create and save animation of tSNE evolution wrt time
+
+    """Plot bar chart of accuracy wrt node degree of trained GAT"""
+    # Get model's classifications
+    _, out = gat(data.x, data.edge_index)
+
+    # Calculate the degree of each node
+    degrees = degree(data.edge_index[0]).numpy()
+
+    # Store accuracy scores and sample sizes
+    accuracies = []
+    sizes = []
+
+    # Accuracy for degrees between 0 and 5
+    for i in range(0, 6):
+        mask = np.where(degrees == i)[0]
+        accuracies.append(accuracy(out.argmax(dim=1)[mask], data.y[mask]))
+        sizes.append(len(mask))
+
+    # Accuracy for degrees > 5
+    mask = np.where(degrees > 5)[0]
+    accuracies.append(accuracy(out.argmax(dim=1)[mask], data.y[mask]))
+    sizes.append(len(mask))
+
+    # Bar plot
+    fig, ax = plt.subplots(figsize=(18, 9))
+    ax.set_xlabel('Node Degree')
+    ax.set_ylabel('Accuracy Score')
+    ax.set_facecolor('#EFEEEA')
+    plt.bar(['0', '1', '2', '3', '4', '5', '>5'],
+            accuracies,
+            color='#0A047A')
+    for i in range(0, 7):
+        plt.text(i, accuracies[i], f'{accuracies[i] * 100:.2f}%', ha='center', color='#0A047A')
+        plt.text(i, accuracies[i] // 2, sizes[i], ha='center', color='white')
+    plt.title('Accuracy Distribution after Training')
+    plt.savefig('accNodeDegreeEpoch'+str(epoch)+'.png')
+    plt.show()
+
+    toc = time.time()
+    print("All simulations completed. Program terminating. Total time taken was",
+          str(datetime.timedelta(seconds=toc - tic)))
